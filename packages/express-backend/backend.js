94301aca1c5878d882528f45f3e9d3b5daa9624d
Dre Suguitan <ddre2026@gmail.com>
2024-10-16 14:29:36 -0700

    After CORS

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 const port = 8000;
@@ -63,6 +64,7 @@ const findUserByNameAndJob = (name, job) => {
     );
 };
 
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
