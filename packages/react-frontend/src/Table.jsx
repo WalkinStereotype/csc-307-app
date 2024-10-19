fe0df5faddab59f875001528ea692727ac7f4808
Dre Suguitan <ddre2026@gmail.com>
2024-10-19 02:04:51 -0700

    Added id column

--- packages/react-frontend/src/Table.jsx
+++ packages/react-frontend/src/Table.jsx
@@ -17,6 +17,7 @@ function TableHeader() {
     return (
       <thead>
         <tr>
+          <th>ID</th>
           <th>Name</th>
           <th>Job</th>
         </tr>
@@ -28,6 +29,7 @@ function TableHeader() {
     const rows = props.characterData.map((row, index) => {
         return (
           <tr key={index}>
+            <td>{row.id}</td>
             <td>{row.name}</td>
             <td>{row.job}</td>
             <td>
